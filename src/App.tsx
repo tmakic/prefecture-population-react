6f32a4e1b8157bdf45329815b2620b8e999cf24a
tmakic <tomakichouen@gmail.com>
2022-12-26 11:00:05 +0900

    eslint, prettier導入

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,57 @@
-import React, { useCallback, useEffect, useState } from 'react';
-import axiosBase from 'axios';
+import React, { useCallback, useEffect, useState } from "react";
+import axiosBase from "axios";
 
-import { Prefecture, InputCheckEvent } from './types'
+import { Prefecture, InputCheckEvent } from "./types";
 
-import './App.css';
+import "./App.css";
 
 const axios = axiosBase.create({
   baseURL: process.env.REACT_APP_RESAS_API_BASE_URL,
   headers: {
-    'X-API-KEY': process.env.REACT_APP_RESAS_API_KEY
+    "X-API-KEY": process.env.REACT_APP_RESAS_API_KEY
   }
-})
+});
 
 function App() {
   const [prefectureList, setPrefectureList] = useState<Prefecture[]>([]);
-  const [selectedPrefectureList, setSelectedPrefectureList] = useState<Prefecture[]>([]);
+  const [selectedPrefectureList, setSelectedPrefectureList] = useState<
+    Prefecture[]
+  >([]);
 
   useEffect(() => {
-    axios.get("/api/v1/prefectures").then(response => {
-      setPrefectureList(response.data.result)
+    axios.get("/api/v1/prefectures").then((response) => {
+      setPrefectureList(response.data.result);
     });
-  }, [])
+  }, []);
 
   const onSelectPrefecture = useCallback((event: InputCheckEvent) => {
-    const selectedPrefecture = prefectureList.find(pref => {
-      return pref.prefCode === Number(event.target?.value)
-    })
+    const selectedPrefecture = prefectureList.find((pref) => {
+      return pref.prefCode === Number(event.target?.value);
+    });
     if (!selectedPrefecture) return;
-    setSelectedPrefectureList([...selectedPrefectureList, selectedPrefecture])
-  }, [])
+    setSelectedPrefectureList([...selectedPrefectureList, selectedPrefecture]);
+  }, []);
 
   return (
     <div className="App">
       <div className="PrefectureList">
-        {prefectureList.map(prefecture => {
+        {prefectureList.map((prefecture) => {
           return (
             <div className="CheckboxWrapper" key={prefecture.prefCode}>
-              <input id={String(prefecture.prefCode)} type="checkbox" value={prefecture.prefCode} onInput={onSelectPrefecture} />
-              <label htmlFor={String(prefecture.prefCode)}>{prefecture.prefName}</label>
+              <input
+                id={String(prefecture.prefCode)}
+                type="checkbox"
+                value={prefecture.prefCode}
+                onInput={onSelectPrefecture}
+              />
+              <label htmlFor={String(prefecture.prefCode)}>
+                {prefecture.prefName}
+              </label>
             </div>
-          )
+          );
         })}
       </div>
-      <div className='Graph'></div>
+      <div className="Graph"></div>
     </div>
   );
 }
